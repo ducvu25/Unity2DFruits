635b79270d991f4935943cc84d6c619672151aff
Trung Đức <100559245+ducvu25@users.noreply.github.com>
2024-03-01 18:59:30 +0700

    Up

--- Fruits/Assets/GameController.cs
+++ Fruits/Assets/GameController.cs
@@ -7,23 +7,29 @@ public class GameController : MonoBehaviour
     [SerializeField] GameObject goFruitPrefabs;
     [SerializeField] Transform pSpawn;
     [SerializeField] float distanceSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Spawn();
-        }
+        
     }
 
-    void Spawn()
+
+    public void Spawn()
     {
-        Instantiate(goFruitPrefabs, pSpawn.position + new Vector3(Random.Range(-distanceSpawn, distanceSpawn), 0, 0), Quaternion.identity);
+        Instantiate(goFruitPrefabs, pSpawn.position, Quaternion.identity);
+    }
+    public void Spawn(Vector3 p1, Vector3 p2, int level)
+    {
+        Vector3 p = (p1 + p2) / 2;
+        GameObject go = Instantiate(goFruitPrefabs, p, Quaternion.identity);
+        go.transform.GetComponent<FruitController>().Level = level;
+
     }
 }
--- Fruits/Assets/Scripts/FruitController.cs
+++ Fruits/Assets/Scripts/FruitController.cs
@@ -6,15 +6,61 @@ using UnityEngine;
 public class FruitController : MonoBehaviour
 {
     int level = 0;
+    bool controlWithPlayer = true;
+    bool spawn = true;
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float distance = 2;
+    Vector3 pInit;
+    public bool isMatch = true;
+    Rigidbody2D rb;
+    float moveX;
+
+
+    GameController controller;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controller = FindAnyObjectByType<GameController>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i < transform.childCount; i++)
+
+        rb.gravityScale = 0;
+        pInit = transform.position;
+        for (int i=0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
         }
         level = Rand();
         transform.GetChild(level).gameObject.SetActive(true);
+    }
+    void Run()
+    {
+        rb.velocity = new Vector2(moveX*speed, 0);
+        if((transform.position.x - pInit.x < -distance && rb.velocity.x < 0) || (transform.position.x - pInit.x > distance && rb.velocity.x > 0))
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+    private void Update()
+    {
+        if (controlWithPlayer)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                moveX = -1;
+            else if (Input.GetKey(KeyCode.RightArrow))
+                moveX = 1;
+            else
+                moveX = 0;
+            Run();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.gravityScale = 1;
+                controlWithPlayer = false;
+            }
+        }
     }
     int Rand()
     {
@@ -27,12 +73,38 @@ public class FruitController : MonoBehaviour
         //return list[Random.RandomRange(0, list.Count)];
         return Random.RandomRange(0, transform.childCount);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FruitController fruitController = collision.transform.GetComponent<FruitController>();
+        if(fruitController != null)
+        {
+            if (collision.transform.CompareTag("Fruits") && fruitController.isMatch && isMatch && !controlWithPlayer && level < 10
+            && fruitController.Level == level)
+            {
+                if (level != 0)
+                {
+                    controller.Spawn(transform.position, collision.transform.position, level + 1);
+                }
+                isMatch = false;
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
+        if (spawn)
+        {
+            controller.Spawn();
+            spawn = false;
+        }
+        
+    }
     public int Level
     {
         get { return level; }
-        set { level = value; 
+        set { level = value;
+            controlWithPlayer = false;
             transform.GetChild(level).gameObject.SetActive(true);
             transform.GetComponent<Animator>().SetTrigger("Zoom");
+            rb.gravityScale = 1;
         }
     }
 
